a595b4fc574cdfb5f5011db280010637270ff600
JustinAMB <Justinmb@outlook.es>
2022-01-19 17:58:17 -0600

    se crea la una funcion para buscar un user por medio del email

--- src/db/user.ts
+++ src/db/user.ts
@@ -4,10 +4,17 @@ import connection from './connection';
 
 
 export const getUser=async (id:number)=>{
-    let user;
+   
     console.log(id);
     const data=await connection.query(`select * from user where id=?`, [id]) as RowDataPacket[][];
     
     return data[0][0];
 }
 
+export const getUserByEmail=async (email:string)=>{
+    
+    console.log(email);
+    const data=await connection.query(`select * from user where email=?`, [email]) as RowDataPacket[][];
+    
+    return data[0][0];
+}
