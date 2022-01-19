eb6dcbc80667f810454651c138667887ee5577f2
JustinAMB <Justinmb@outlook.es>
2022-01-18 21:03:29 -0600

    se crea la conexion con la base de datos

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -7,9 +7,17 @@ const pool:Pool=mysql.createPool({
     host: 'localhost',
     user: 'root',
     password: 'jamb116',
-    database: 'localizacioncr',
+    database: 'puntoventa',
     insecureAuth: true,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
+pool.getConnection((err, connection) =>{
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('Connected to database');
+    }
+});
+export default pool;
