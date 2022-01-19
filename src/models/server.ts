6b6dbc9d8490e5f01e427b074c35d0f861ab6a93
JustinAMB <Justinmb@outlook.es>
2022-01-18 21:29:14 -0600

    se crea el sservidor y se configura los middlewares y  el puesto

---
+++ src/models/server.ts
@@ -0,0 +1,34 @@
+import express,{Application} from 'express';
+
+import cors from 'cors';
+
+import morgan from 'morgan';
+
+class Server{
+    private app!: Application;
+    private port!: string;
+    private apiPaths={}
+    constructor(){
+        this.app=express();
+        this.port=process.env.PORT || '73';
+        this.middlewares();
+        this.routes();
+        
+    }
+
+    middlewares(){
+        this.app.use(morgan('dev'));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use( express.static('public') );
+    }
+    routes(){
+        console.log('routess');
+    }
+    listen(){
+        this.app.listen(this.port,()=>{
+            console.log(`Server running on port ${this.port}`);
+        });
+
+    }
+}
