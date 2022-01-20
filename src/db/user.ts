53848577301d40bf4f9a7552e76186286ae1caa0
JustinAMB <Justinmb@outlook.es>
2022-01-19 18:16:41 -0600

    se realizan cambios de tipado en retorno de la funciones

--- src/db/user.ts
+++ src/db/user.ts
@@ -1,20 +1,21 @@
 import { RowDataPacket } from 'mysql2';
+import User from '../models/user.model';
 import connection from './connection';
 
 
 
-export const getUser=async (id:number)=>{
+export const getUser=async (id:number):Promise<User>=>{
    
     console.log(id);
     const data=await connection.query(`select * from user where id=?`, [id]) as RowDataPacket[][];
     
-    return data[0][0];
+    return data[0][0] as User;
 }
 
-export const getUserByEmail=async (email:string)=>{
+export const getUserByEmail=async (email:string):Promise<User>=>{
     
     console.log(email);
     const data=await connection.query(`select * from user where email=?`, [email]) as RowDataPacket[][];
     
-    return data[0][0];
+    return data[0][0]  as User;
 }
