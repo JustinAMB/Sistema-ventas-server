bfaa7f17fd5bb2f5bd64031c542725dfccba94d0
JustinAMB <Justinmb@outlook.es>
2022-01-22 22:23:50 -0600

    se implementa en la ruta de person  el middleware para validar el id

--- src/routes/person.routes.ts
+++ src/routes/person.routes.ts
@@ -1,13 +1,14 @@
 import {Router} from 'express';
 import { addPerson, getAllPerson, getPersonById, updatePersonById } from '../controllers/person.controller';
 import { isAdmin, verifyToken } from '../middleware/authJwt';
+import { verifyIdPerson } from '../middleware/verifyId';
 
 const router=Router();
 
-router.get('/:id',[verifyToken,isAdmin],getPersonById);
+router.get('/:id',[verifyToken,isAdmin,verifyIdPerson],getPersonById);
 router.get('/:kind',[verifyToken,isAdmin],getAllPerson);
 router.post('/:kind',[verifyToken,isAdmin],addPerson);
-router.put('/:id',[verifyToken,isAdmin],updatePersonById);
+router.put('/:id',[verifyToken,isAdmin,verifyIdPerson],updatePersonById);
 
 
 export default router;
