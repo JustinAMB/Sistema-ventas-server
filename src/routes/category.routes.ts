6ad042be1c1a46b076677afd4deb80641a7b1450
JustinAMB <Justinmb@outlook.es>
2022-01-22 22:21:49 -0600

    se implementa en la ruta de category el middleware para validar el id

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,16 +1,22 @@
 import {Router} from 'express';
-import { addCategory, updateCategoryById } from "./../controllers/category.controller";
-import { getCategoryById ,getAllCategory } from '../controllers/category.controller';
+
+import { 
+    getCategoryById ,
+    getAllCategory,
+    addCategory, 
+    updateCategoryById 
+} from '../controllers/category.controller';
 import { isAdmin, verifyToken } from '../middleware/authJwt';
+import { verifyIdCategory } from '../middleware/verifyId';
 
 const router=Router();
 
 
 
-router.get('/:id',[verifyToken,isAdmin],getCategoryById);
+router.get('/:id',[verifyToken,isAdmin,verifyIdCategory],getCategoryById);
 router.get('/',[verifyToken,isAdmin],getAllCategory);
 router.post('/',[verifyToken,isAdmin],addCategory);
-router.put('/:id',[verifyToken,isAdmin],updateCategoryById);
+router.put('/:id',[verifyToken,isAdmin,verifyIdCategory],updateCategoryById);
 
 
 export default router;
