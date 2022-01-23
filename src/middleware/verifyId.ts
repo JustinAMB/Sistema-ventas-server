e9a1f8667e7455f497b932dad70f260817e93031
JustinAMB <Justinmb@outlook.es>
2022-01-22 22:10:14 -0600

    se crea un middkeware para validar si existe una categoria  por medio del id

--- src/middleware/verifyId.ts
+++ src/middleware/verifyId.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { getCategory } from "../db/category";
 import { getProduct } from "../db/product";
 
 export const verifyIdProduct = async (req:Request, res:Response, next:Function) => {
@@ -11,6 +12,28 @@ export const verifyIdProduct = async (req:Request, res:Response, next:Function) 
             return res.status(403).json({ 
                 ok: false,
                 message: 'Product not found.'
+          });
+        }
+        next();
+  
+      
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({ ok:false,message: error });
+    }
+};
+
+
+export const verifyIdCategory = async (req:Request, res:Response, next:Function) => {
+    try {
+        const { id } = req.params;
+        const category = await getCategory(Number(id));
+      
+      
+        if (!category) {
+            return res.status(403).json({ 
+                ok: false,
+                message: 'category not found.'
           });
         }
         next();
