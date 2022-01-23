0dd04d423891d833dd341ea2dbdf3461d6bf3e77
JustinAMB <Justinmb@outlook.es>
2022-01-22 22:25:05 -0600

    se implementa en la ruta de producto  el middleware para validar el id

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -3,16 +3,17 @@ import {Router} from 'express';
 
 import { isAdmin, verifyToken } from '../middleware/authJwt';
 import { activeProductbyId, addProduct, getAllProduct, getProductById, updateProductById } from '../controllers/product.controller';
+import { verifyIdProduct } from '../middleware/verifyId';
 
 const router=Router();
 
 
 
-router.get('/:id',[verifyToken],getProductById);
+router.get('/:id',[verifyToken,verifyIdProduct],getProductById);
 router.get('/',[verifyToken],getAllProduct);
 router.post('/',[verifyToken,isAdmin],addProduct);
-router.post('/:id',[verifyToken,isAdmin],activeProductbyId);
-router.put('/:id',[verifyToken,isAdmin],updateProductById);
+router.post('/:id',[verifyToken,isAdmin,verifyIdProduct],activeProductbyId);
+router.put('/:id',[verifyToken,isAdmin,verifyIdProduct],updateProductById);
 
 
 export default router;
