6991769184d3391b01073be382d19fc8384462a3
JustinAMB <Justinmb@outlook.es>
2022-01-24 15:22:10 -0600

    se crea el el parametro kind en createSell

--- src/db/sell.ts
+++ src/db/sell.ts
@@ -13,8 +13,8 @@ export const getSell=async(id:number):Promise<Sell>=>{
     return data[0][0] as Sell;
 } 
 
-export const createSell=async(sell:Sell):Promise<Response>=>{
+export const createSell=async(kind:number,sell:Sell):Promise<Response>=>{
     const {person,total,user}=sell;
-    const data =await connection.query('call createSell(?,?,?)',[user,person,total]) as RowDataPacket[][];
+    const data =await connection.query('call createSell(?,?,?,?)',[kind,user,person,total]) as RowDataPacket[][];
     return data[0][0] as Response;
 }
