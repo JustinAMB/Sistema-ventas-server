2bd10a0fd3980c30496bee15062145789f745972
JustinAMB <Justinmb@outlook.es>
2022-01-24 17:59:58 -0600

    se crea la funcion para que la base de datos cree un user

--- src/db/user.ts
+++ src/db/user.ts
@@ -1,4 +1,5 @@
 import { RowDataPacket } from 'mysql2';
+import Response from '../models/response.model';
 import User from '../models/user.model';
 import connection from './connection';
 
@@ -18,4 +19,12 @@ export const getUserByEmail=async (email:string):Promise<User>=>{
     const data=await connection.query(`select * from user where email=?`, [email]) as RowDataPacket[][];
     
     return data[0][0]  as User;
+}
+
+
+
+export const createUser=async (user:User):Promise<Response>=>{
+    const {name,email,lastname,rol,image,password}=user;
+    const data= await connection.query(`call CreateUser(?,?,?,?,?,?)`,[name,lastname,email,password,image,rol]) as RowDataPacket[][] ;
+    return data[0][0] as Response;
 }
