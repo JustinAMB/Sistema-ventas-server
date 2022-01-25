5f04d19b7907ace2a272037dca9759055d9cb327
JustinAMB <Justinmb@outlook.es>
2022-01-24 23:53:25 -0600

    se agregan mas rutas al router de user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,8 @@
 import {Router} from 'express';
-import { singIn } from '../controllers/user.controller';
+import { activeUserById, addUser, getAllUsers, getUserById, singIn, updateUserById } from '../controllers/user.controller';
 
-
+import {  isAdmin, verifyToken } from '../middleware/authJwt';
+import { verifyIdUser } from '../middleware/verifyId';
 const router=Router();
 
 
@@ -9,7 +10,11 @@ const router=Router();
 
 router.post('/signIn',singIn);
 
-
+router.post('/',[verifyToken,isAdmin],addUser);
+router.put('/:id',[verifyToken,isAdmin,verifyIdUser],updateUserById);
+router.get('/',[verifyToken,isAdmin],getAllUsers);
+router.get('/:id',[verifyToken,isAdmin,verifyIdUser],getUserById);
+router.put('/active/:id',[verifyToken,isAdmin,verifyIdUser],activeUserById);
 
 export default router;
 
