443db57ef9d63a7f5efa8db7a970324d89976641
JustinAMB <Justinmb@outlook.es>
2022-01-24 23:13:59 -0600

    se realiza un middleware para verificar el id de un user

--- src/middleware/verifyId.ts
+++ src/middleware/verifyId.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { getCategory } from "../db/category";
 import { getPerson } from "../db/person";
 import { getProduct } from "../db/product";
+import { getUser } from "../db/user";
 
 export const verifyIdProduct = async (req:Request, res:Response, next:Function) => {
     try {
@@ -67,4 +68,25 @@ export const verifyIdPerson = async (req:Request, res:Response, next:Function) =
       console.log(error);
       return res.status(500).send({ ok:false,message: error });
     }
+};
+
+export const verifyIdUser = async (req:Request, res:Response, next:Function) => {
+  try {
+      const { id } = req.params;
+      const user = await getUser(Number(id));
+    
+    
+      if (!user) {
+          return res.status(403).json({ 
+              ok: false,
+              message: 'user not found.'
+        });
+      }
+      next();
+
+    
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ ok:false,message: error });
+  }
 };
