730767d3b6a4bc377f7456e2fe182455569c81fd
JustinAMB <Justinmb@outlook.es>
2022-01-26 22:04:45 -0600

    se le cambia el nombre a una ruta de person

--- src/routes/person.routes.ts
+++ src/routes/person.routes.ts
@@ -6,7 +6,7 @@ import { verifyIdPerson } from '../middleware/verifyId';
 const router=Router();
 
 router.get('/:id',[verifyToken,isAdmin,verifyIdPerson],getPersonById);
-router.get('/:kind',[verifyToken,isAdmin],getAllPerson);
+router.get('/getPersons/:kind',[verifyToken,isAdmin],getAllPerson);
 router.post('/:kind',[verifyToken,isAdmin],addPerson);
 router.put('/:id',[verifyToken,isAdmin,verifyIdPerson],updatePersonById);
 
