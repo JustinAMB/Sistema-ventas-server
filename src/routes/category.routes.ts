101060df60e81ab23c1ad4e30521fe63808ae90e
JustinAMB <Justinmb@outlook.es>
2022-01-29 22:21:11 -0600

    se crea la ruta para activar category

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -4,7 +4,8 @@ import {
     getCategoryById ,
     getAllCategory,
     addCategory, 
-    updateCategoryById 
+    updateCategoryById ,
+    activeCategoryById
 } from '../controllers/category.controller';
 import { isAdmin, verifyToken } from '../middleware/authJwt';
 import { verifyIdCategory } from '../middleware/verifyId';
@@ -17,6 +18,7 @@ router.get('/:id',[verifyToken,isAdmin,verifyIdCategory],getCategoryById);
 router.get('/',[verifyToken,isAdmin],getAllCategory);
 router.post('/',[verifyToken,isAdmin],addCategory);
 router.put('/:id',[verifyToken,isAdmin,verifyIdCategory],updateCategoryById);
+router.put('/active/:id',[verifyToken,isAdmin,verifyIdCategory],activeCategoryById);
 
 
 export default router;
