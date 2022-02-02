1bccb9bc7dd4e369737dad03f55f584d7780979b
JustinAMB <Justinmb@outlook.es>
2022-02-01 21:11:29 -0600

    se crea la ruta para activePerson

--- src/routes/person.routes.ts
+++ src/routes/person.routes.ts
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { addPerson, getAllPerson, getPersonById, updatePersonById } from '../controllers/person.controller';
+import { activePersonById, addPerson, getAllPerson, getPersonById, updatePersonById } from '../controllers/person.controller';
 import { isAdmin, verifyToken } from '../middleware/authJwt';
 import { verifyIdPerson } from '../middleware/verifyId';
 
@@ -9,6 +9,6 @@ router.get('/:id',[verifyToken,isAdmin,verifyIdPerson],getPersonById);
 router.get('/getPersons/:kind',[verifyToken,isAdmin],getAllPerson);
 router.post('/:kind',[verifyToken,isAdmin],addPerson);
 router.put('/:id',[verifyToken,isAdmin,verifyIdPerson],updatePersonById);
-
+router.put('/active/:id',[verifyToken,isAdmin,verifyIdPerson],activePersonById);
 
 export default router;
