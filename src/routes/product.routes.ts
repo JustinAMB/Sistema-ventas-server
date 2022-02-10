88338a933b9076a783417c272b24fadd55f70bc5
JustinAMB <Justinmb@outlook.es>
2022-02-10 00:31:19 -0600

    se crea la ruta de buscar inventary

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -2,7 +2,7 @@ import {Router} from 'express';
 
 
 import { isAdmin, verifyToken } from '../middleware/authJwt';
-import { activeProductbyId, addProduct, getAllProduct, getProductById, updateProductById } from '../controllers/product.controller';
+import { activeProductbyId, addProduct, getAllProduct, getProductById, searchInventaryByTerm, updateProductById } from '../controllers/product.controller';
 import { verifyIdProduct } from '../middleware/verifyId';
 
 const router=Router();
@@ -11,6 +11,7 @@ const router=Router();
 
 router.get('/:id',[verifyToken,verifyIdProduct],getProductById);
 router.get('/',[verifyToken],getAllProduct);
+router.get('/searchInventary/',[verifyToken],searchInventaryByTerm);
 router.post('/',[verifyToken,isAdmin],addProduct);
 router.post('/:id',[verifyToken,isAdmin,verifyIdProduct],activeProductbyId);
 router.put('/:id',[verifyToken,isAdmin,verifyIdProduct],updateProductById);
