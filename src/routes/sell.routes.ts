03290449c8b9418854d0196822b5c4bb04c12512
JustinAMB <Justinmb@outlook.es>
2022-02-10 22:29:47 -0600

    se implementa el middleware verifyEmailBySell en la ruta post de sell

--- src/routes/sell.routes.ts
+++ src/routes/sell.routes.ts
@@ -2,11 +2,12 @@ import {Router} from 'express';
 
 import {  verifyToken } from '../middleware/authJwt';
 import { addSell } from '../controllers/sell.controller';
+import { verifyEmailBySell } from '../middleware/verifyId';
 
 const router=Router();
 
 
-router.post('/:kind',[verifyToken],addSell);
+router.post('/:kind',[verifyToken,verifyEmailBySell],addSell);
 
 
 
