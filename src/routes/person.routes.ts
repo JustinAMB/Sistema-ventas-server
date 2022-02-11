86114a9f6ef4f8348c7449bc426bfd2d0d9c4864
JustinAMB <Justinmb@outlook.es>
2022-02-10 22:16:09 -0600

    se agrega los middleware a sus respectivas ruta

--- src/routes/person.routes.ts
+++ src/routes/person.routes.ts
@@ -1,13 +1,14 @@
 import {Router} from 'express';
 import { activePersonById, addPerson, getAllPerson, getPersonById, updatePersonById } from '../controllers/person.controller';
 import { isAdmin, verifyToken } from '../middleware/authJwt';
+import { emailValidator } from '../middleware/unique';
 import { verifyIdPerson } from '../middleware/verifyId';
 
 const router=Router();
 
 router.get('/:id',[verifyToken,isAdmin,verifyIdPerson],getPersonById);
 router.get('/getPersons/:kind',[verifyToken,isAdmin],getAllPerson);
-router.post('/:kind',[verifyToken,isAdmin],addPerson);
+router.post('/:kind',[verifyToken,isAdmin,emailValidator],addPerson);
 router.put('/:id',[verifyToken,isAdmin,verifyIdPerson],updatePersonById);
 router.put('/active/:id',[verifyToken,isAdmin,verifyIdPerson],activePersonById);
 
--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -4,6 +4,7 @@ import {Router} from 'express';
 import { isAdmin, verifyToken } from '../middleware/authJwt';
 import { activeProductbyId, addProduct, getAllProduct, getProductById, searchInventaryByTerm, updateProductById } from '../controllers/product.controller';
 import { verifyIdProduct } from '../middleware/verifyId';
+import { barcodeValidator } from '../middleware/unique';
 
 const router=Router();
 
@@ -12,7 +13,7 @@ const router=Router();
 router.get('/:id',[verifyToken,verifyIdProduct],getProductById);
 router.get('/',[verifyToken],getAllProduct);
 router.get('/searchInventary/',[verifyToken],searchInventaryByTerm);
-router.post('/',[verifyToken,isAdmin],addProduct);
+router.post('/',[verifyToken,isAdmin,barcodeValidator],addProduct);
 router.post('/:id',[verifyToken,isAdmin,verifyIdProduct],activeProductbyId);
 router.put('/:id',[verifyToken,isAdmin,verifyIdProduct],updateProductById);
 
