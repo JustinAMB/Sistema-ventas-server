62119353286b8cea45d702d045a48719ef221c86
JustinAMB <Justinmb@outlook.es>
2022-02-17 01:19:04 -0600

    se agrega la ruta de getSell

--- src/routes/sell.routes.ts
+++ src/routes/sell.routes.ts
@@ -1,13 +1,14 @@
 import {Router} from 'express';
 
 import {  verifyToken } from '../middleware/authJwt';
-import { addSell } from '../controllers/sell.controller';
+import { addSell, getSellById } from '../controllers/sell.controller';
 import { verifyEmailBySell } from '../middleware/verifyId';
 
 const router=Router();
 
 
 router.post('/:kind',[verifyToken,verifyEmailBySell],addSell);
+router.get('/:id',[verifyToken],getSellById);
 
 
 
