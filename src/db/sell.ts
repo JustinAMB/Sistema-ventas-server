189517758391801b2db097c41292d5e89a7293d2
JustinAMB <Justinmb@outlook.es>
2022-02-17 00:04:05 -0600

    se modifica el return de createSell

--- src/db/sell.ts
+++ src/db/sell.ts
@@ -13,8 +13,8 @@ export const getSell=async(id:number):Promise<Sell>=>{
     return data[0][0] as Sell;
 } 
 
-export const createSell=async(kind:number,sell:Sell):Promise<Response>=>{
+export const createSell=async(kind:number,sell:Sell):Promise<Sell>=>{
     const {person,total,user}=sell;
     const data =await connection.query('call createSell(?,?,?,?)',[kind,user,person,total]) as RowDataPacket[][];
-    return data[0][0][0] as Response;
+    return data[0][0][0] as Sell;
 }
