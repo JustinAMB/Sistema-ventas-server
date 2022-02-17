ba08656659498c3e205b096ef3de2e7feb0ad2f1
JustinAMB <Justinmb@outlook.es>
2022-02-17 00:34:38 -0600

    se crea una funcion para obtener una  una venta y sus detales por medio del id

--- src/controllers/sell.controller.ts
+++ src/controllers/sell.controller.ts
@@ -1,5 +1,5 @@
 import { Request,Response } from "express";
-import { createSellDetail } from "../db/dellDetail";
+import { createSellDetail, getAllSellDetails, getSell } from "../db/dellDetail";
 import { createSell } from "../db/sell";
 import Sell from "../models/sell.model";
 import SellDetail from "../models/sellDetail.model";
@@ -11,16 +11,16 @@ export const addSell=async(req:Request,res:Response)=>{
         const {person,total,user}=req.body;
         const detailsReq=req.body.details as Array<any>;
         const newSell:Sell={ person,user,total };
-        const {exito}=await createSell(Number(kind),newSell);
+        const resp=await createSell(Number(kind),newSell);
 
-        if(!exito){
+        if(!resp){
             return res.status(401).json({
                 ok:false,
                 message:'Se no se a podido realizado la operacion, por favor intente de nuevo'
             });
         }
         
-
+        const {id:exito}=resp;
         const details=detailsReq.map((detail)=>{
             return {
                 ...detail,
@@ -45,13 +45,13 @@ export const addSell=async(req:Request,res:Response)=>{
             });
         }
        
-        newSell.id=exito;
+        
             console.table(details);
         return res.status(200).json({
             ok:true,
             message:'Se ha realizado la operacion satisfactoriamente',
             data:{
-                sell:newSell,
+                sell:resp,
                 details
             }
         });
@@ -61,5 +61,32 @@ export const addSell=async(req:Request,res:Response)=>{
             ok:false,
             message:'Error inesperado',
         })
+    }
+}
+
+export const getSellById=async(req:Request,res:Response)=>{
+    try{
+        const {id}=req.params;
+        const sell=await getSell(Number(id));
+        const details=await getAllSellDetails(Number(id));
+        if(!sell){
+            return res.status(404).json({
+                ok:false,
+                message:'No se encontro la venta'
+            });
+        }
+        return res.status(200).json({
+            ok:true,
+            data:{
+                sell,
+                details
+            }
+        });
+    } catch(err){
+        res.status(500).json({
+            ok:false,
+            message:'Error inesperado'
+        });
+
     }
 }
