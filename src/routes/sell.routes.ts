74478ae20595408b6dc65e5d991ce337827cba46
JustinAMB <Justinmb@outlook.es>
2022-02-17 01:43:44 -0600

    se agrega la ruta de ventas por dia

--- src/routes/sell.routes.ts
+++ src/routes/sell.routes.ts
@@ -1,14 +1,16 @@
 import {Router} from 'express';
 
 import {  verifyToken } from '../middleware/authJwt';
-import { addSell, getSellById } from '../controllers/sell.controller';
+import { addSell, getSellById, getSellsByDay } from '../controllers/sell.controller';
 import { verifyEmailBySell } from '../middleware/verifyId';
 
 const router=Router();
 
 
 router.post('/:kind',[verifyToken,verifyEmailBySell],addSell);
+router.get('/sellsDay/',[verifyToken],getSellsByDay);
 router.get('/:id',[verifyToken],getSellById);
+
 
 
 
